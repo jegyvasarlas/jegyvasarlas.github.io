ea1d209e1bbf48bedd5944a3f0e3efa95baedeaa
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2021-08-11 07:08:44 +0200

    Delete Untitled-18.js

--- Untitled-18.js
+++
@@ -1,5 +0,0 @@
-function home(){window.open("index.html","_self");};
-function info(){window.open("info.html","_self");};
-function telo(){window.open("telo.html","_self");};
-function berlet(){window.open("berlet.html","_self");};
-function web(){window.open("web.html","_self");};
