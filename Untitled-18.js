53ad486d543be4d852af83580655c3fa90f160cf
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2021-08-26 11:36:30 +0200

    rename_update

--- Untitled-18.js
+++ Untitled-18.js
@@ -1,5 +1,5 @@
 function home(){window.open("index.html","_self");};
 function info(){window.open("info.html","_self");};
-function telo(){window.open("telo.html","_self");};
-function berlet(){window.open("berlet.html","_self");};
+function telo(){window.open("app1.html","_self");};
+function berlet(){window.open("app2.html","_self");};
 function web(){window.open("web.html","_self");};
