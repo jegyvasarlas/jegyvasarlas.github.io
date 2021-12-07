4b62b6204e0eeade96c8052294902350a89bd742
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2021-12-07 09:29:29 +0100

    final

--- Untitled-18.js
+++ Untitled-18.js
@@ -3,3 +3,8 @@ function info(){window.open("info.html","_self");};
 function telo(){window.open("app1.html","_self");};
 function berlet(){window.open("app2.html","_self");};
 function web(){window.open("web.html","_self");};
+mybutton = document.getElementById("myBtn");
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
